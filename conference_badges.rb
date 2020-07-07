03eb9c1757f411fa0addcefa35e22c967f898e39
Cristian Deleon <libaration@gmail.com>
2020-07-07 06:08:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,5 @@ end
 
 def assign_rooms(array)
   ##// array.each_with_index { |name, index| p "#{name}:#{item}" } print name:index
+  array.each_with_index { |name, index| p "Hello, #{name}!You'll be assigned to room#{item}" } 
 end
