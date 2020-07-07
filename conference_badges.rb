fe7d207ae7555bc7f48ef5723b78d96d4b9553b0
Cristian Deleon <libaration@gmail.com>
2020-07-07 06:33:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,7 @@ end
 def assign_rooms(array)
   assigned = []
   ##// array.each_with_index { |name, index| p "Hello, #{name}! You'll be assigned to room #{index}" } ##//put name:index assignment
-  array.each_with_index { |item, index| assigned.push index }
-  return "Youll be assigned to room #{index}!"
+  ##array.each_with_index { |name, index| assigned.push index }
+  ##array.each { |name| p "Hello, #{name}! You'll be assigned to room" }
+  array.each_with_index { |name, index| p "Hello, #{name}! You'll be assigned to room #{index}" }
 end
